0cb22cda31d22aa18a9f534c3c0ab69004835c07
Wolfgang Wiese <xwolf@xwolf.de>
2025-01-04 21:08:23 +0100

    Anpassen des Outputs an Objecttypen

--- src/Post.php
+++ src/Post.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * Copyright (C) 2024 xwolf
+ * Copyright (C) 2025 xwolf
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
